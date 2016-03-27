dbde8d09a69eaed7378051d4c17a2cf9deaba386
gobi <gabrielli.george@gmail.com>
2016-03-27 15:30:36 +0300

    Wallpaper changing script works

--- scripts/setwallpaper.sh
+++ scripts/setwallpaper.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-wallpaperdir='$HOME/wallpaper'
 
-files=($wallpaperdir/*)
-randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"`
+#Choose a random image from the path
+path="$HOME/Pictures/Wallpapers"
 
-gconftool-2 -t str --set /desktop/gnome/background/picture_filename "$randompic"
+#Writing to /tmp instead of /var/run to simplify deployment
+pid_file="/tmp/setwallpaper.pid"
+
+if [ -s $pid_file ]; then
+	read pid < $pid_file
+	if [ ! -z $pid ]; then
+		kill -s 0 "$pid" && kill -s 9 "$pid"
+	fi
+fi
+
+echo "$$" > $pid_file
+
+while true; do
+	find $path/ -type f \( -iname '*.jpg' -o -iname '*.png' \) | shuf -n1 | xargs feh --bg-scale
+	sleep 15m
+done
