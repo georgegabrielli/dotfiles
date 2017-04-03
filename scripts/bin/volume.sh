b38fff2b9ca4cd58ef98e6ccda8ad0ce9a240a61
Gabrielli George <gabrielli.george@gmail.com>
2017-04-03 12:39:11 +0300

    Volnoti for volume.. need to change the config file

--- scripts/bin/volume.sh
+++ scripts/bin/volume.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+[[ $1 == "up" ]] && amixer set Master 5%+
+[[ $1 == "down" ]] && amixer set Master 5%-
+[[ $1 == "mute" ]] && amixer set Master toggle
 volume=$(amixer -D pulse get Master | grep -o "\[.*%\]" | grep -o "[0-9]*" | head -n1)
 
-echo " $volume "
+echo $volume
+volnoti-show  $volume 
