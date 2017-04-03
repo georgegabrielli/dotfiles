82363d367a216a1cb38b5cd773c0a92c224e4fc6
Gabrielli George <gabrielli.george@gmail.com>
2017-04-03 12:43:16 +0300

    Config added

--- scripts/bin/volume.sh
+++ scripts/bin/volume.sh
@@ -5,6 +5,6 @@
 [[ $1 == "down" ]] && amixer set Master 5%-
 [[ $1 == "mute" ]] && amixer set Master toggle
 volume=$(amixer -D pulse get Master | grep -o "\[.*%\]" | grep -o "[0-9]*" | head -n1)
-
+[[ $(amixer -D pulse get Master | grep "\[off\]") ]] && volnoti-show -m $volume && exit
 echo $volume
 volnoti-show  $volume 
